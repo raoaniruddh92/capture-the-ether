12355937efd70d9a1d21bf5d25418a1b5bb316e5
Aniruddh Rao <raoaniruddh92@gmail.com>
2025-12-08 02:09:42 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,17 @@ function Home() {
       <div className="scan-lines"></div>
 
       <h1 className="title">GhostLedger</h1>
-      <p className="subtitle">Blockchain security challenges</p>
+      <p className="subtitle">Blockchain security challenges-A collection of all the challenges on different sites you can't play anymore</p>
+
+
+      <p className="subtitle">I made this because capture the ether has been depracated for a while now ;( </p>
+      <p className="subtitle" >This project is fully open source so feel free to contribute</p>
 
       <Link to="/challenges">
         <button className="enter-btn">ENTER CHALLENGES</button>
       </Link>
 
-      <p className="footer-text">Protocol v1.0 — Unauthorized access is forbidden</p>
+      <p className="footer-text">Protocol v1.0 — Unauthorized access is forbidden-d3cod3rd3m0n</p>
     </div>
   );
 }
